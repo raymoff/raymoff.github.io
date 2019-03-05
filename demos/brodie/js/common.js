dec37040ab6f5ada15a0276846e6776c099e2230
Adilet Uraimov <uraimoff@yahoo.com>
2019-03-05 09:38:08 +0600

    Tools page

--- demos/brodie/js/common.js
+++ demos/brodie/js/common.js
@@ -1,7 +1,19 @@
 $(function() {
 
   // Custom JS
-  
+
+  // Preloader
+  setTimeout(function() {
+    $('body').addClass('loaded');
+  });
+
+  // Table search
+  $('#toolsInput').on('keyup', function() {
+    var value = $(this).val().toLowerCase();
+    $('#toolsTable tr').filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 
   // Stop image drag
   $("img, a").on("dragstart", function (event) {
